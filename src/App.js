66bf82f87012a91bc02dfce1147c0b54a2414953
Sunil Shelke <shelkesunil072@gmail.com>
2025-01-04 10:07:39 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 
 function App() {
   return (
