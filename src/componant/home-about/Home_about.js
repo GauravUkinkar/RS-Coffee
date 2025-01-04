00e679ebeb52a546fadb488582a1e84d455a93a3
Sunil Shelke <shelkesunil072@gmail.com>
2025-01-04 17:17:22 +0530

    new

--- src/componant/home-about/Home_about.js
+++ src/componant/home-about/Home_about.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import "./home-about.scss"
+import React from "react";
+import "./home-about.scss";
 const Home_about = () => {
   return (
-  <>
-  <div className="home-about-parent parent">
-    <div className="home-about-cont cont">
-<div className="home-about-left"></div>
-<div className="home-about-right">
-  <h2 className="home-about-heading">Professional Roast Master: Justus du Toit (SCA)</h2>
-  <p className='home-about-desc'>All WCR "HANGARS" as they are called, are owned by professionals in either the engineering or aviation fields, making it truly a collection of very technical minds to source, roast and supply the freshest possible coffee to small markets that normally would not have access to such a premium quality product.</p>
-</div>
-    </div>
-  </div>
-  </>
-  )
-}
+    <>
+      <div className="home-about-parent parent">
+        <div className="home-about-cont cont">
+          <div className="home-about-left bg-img-cover"></div>
+          <div className="home-about-right">
+            <h2 className="home-about-heading">
+              Professional Roast Master: Justus du Toit (SCA)
+            </h2>
+            <p className="home-about-desc">
+              All WCR "HANGARS" as they are called, are owned by professionals
+              in either the engineering or aviation fields, making it truly a
+              collection of very technical minds to source, roast and supply the
+              freshest possible coffee to small markets that normally would not
+              have access to such a premium quality product.
+            </p>
+            <div className="btn">Read More</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home_about
+export default Home_about;
