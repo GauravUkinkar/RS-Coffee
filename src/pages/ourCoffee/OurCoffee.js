2e6939378cda7c4110d87d1f7224eafe46ef64c1
Rishabh Khade <89901588+rishabhkhade@users.noreply.github.com>
2025-01-04 14:44:40 +0530

    ok

---
+++ src/pages/ourCoffee/OurCoffee.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ourCoffee.scss";
+
+function OurCoffee() {
+  return (
+    <>
+      <div className="parent parent-our-coffee">
+        <div className="cont cont-our-coffee">
+          <div class="left-our-coffee ">
+            <div class="img-coffee-boy bg-img-cover"></div>
+          </div>
+          <div class="right-our-coffee">
+            <h2>At WCR we source our coffee directly from Origin.</h2>
+            <p>Our direct sourcing principal allows us to work closer with the producers. This has the following benefits for our clients:</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default OurCoffee;
