d0dc1d7f2ac1b8097292944e88ffeb7c45b24978
Rishabh Khade <89901588+rishabhkhade@users.noreply.github.com>
2025-01-04 16:50:40 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/home/Home";
+import OurCoffee from "./pages/ourCoffee/OurCoffee";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home /> } />
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/our-coffee" element={<OurCoffee />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+ 
--- src/pages/ourCoffee/OurCoffee.js
+++ src/pages/ourCoffee/OurCoffee.js
@@ -11,7 +11,26 @@ function OurCoffee() {
           </div>
           <div class="right-our-coffee">
             <h2>At WCR we source our coffee directly from Origin.</h2>
-            <p>Our direct sourcing principal allows us to work closer with the producers. This has the following benefits for our clients:</p>
+            <p>
+              Our direct sourcing principal allows us to work closer with the
+              producers. This has the following benefits for our clients:
+            </p>
+            <div class="two-points">
+              <div class="bullet">
+                <span>.</span>
+                <p>
+                  Fixed prices for at least 6 months due to direct relationship
+                  with farmers
+                </p>
+              </div>
+              <div class="bullet">
+                <span>.</span>
+                <p>
+                  Consistent quality guaranteed due to origin visit’s before
+                  shipment dispatch to Qatar
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
