2574b90fcb19486e02f47d76611cbf41fefb3a03
Sunil Shelke <shelkesunil072@gmail.com>
2025-01-04 10:04:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Coffee</h2>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home /> } />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
