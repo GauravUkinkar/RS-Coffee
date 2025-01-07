65f5b685842614024f4e80ba7be5cb852e18cf92
Sunil Shelke <shelkesunil072@gmail.com>
2025-01-07 12:35:48 +0530

    new

--- src/componant/home-product/Home_product.js
+++ src/componant/home-product/Home_product.js
@@ -1,85 +1,164 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./home-product.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"; // Import core Swiper styles
 import "swiper/css/pagination"; // Import Swiper Pagination styles
-import { Pagination } from "swiper/modules"; // Import Pagination module
+import "swiper/css/autoplay";
+import { Pagination, Autoplay } from "swiper/modules";
 import { Link } from "react-router-dom";
-import coffee1 from "../../assets/product/coffee-1.png"
-import coffee2 from "../../assets/product/coffee-2.png"
+import coffee1 from "../../assets/product/coffee-1.png";
+import coffee2 from "../../assets/product/coffee-2.png";
+import coffee3 from "../../assets/product/Wings-9-scaled.webp";
+import coffee4 from "../../assets/product/Wings-10-scaled.webp";
+// import { MdFavoriteBorder } from "react-icons/md";
+// import { IoIosSearch } from "react-icons/io";
 const Home_product = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const swiperRef = useRef(null);
   const products = [
     {
       id: 1,
-      img1: coffee1,
-      img2: coffee2,
-      product_title: "Product One",
-      product_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, repellendus.",
-      product_price: "Rs. 280.00",
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "A-10 Blend",
+      product_desc: "Coffee",
+      product_price: "42 – ر.ق140",
+      product_detail: "link1",
     },
     {
       id: 2,
-      img1: "image2.jpg",
-      img2: "image2-hover.jpg",
-      product_title: "Product Two",
-      product_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, repellendus.",
-      product_price: "Rs. 320.00",
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Brazil – Diamond",
+      product_desc: "Coffee",
+      product_price: "42 – ر.ق140",
+      product_detail: "link2",
     },
     {
       id: 3,
-      img1: "image3.jpg",
-      img2: "image3-hover.jpg",
-      product_title: "Product Three",
-      product_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, repellendus.",
-      product_price: "Rs. 150.00",
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Decaf Coffee",
+      product_desc: "Coffee",
+      product_price: "45 – ر.ق150",
+      product_detail: "link3",
     },
     {
       id: 4,
-      img1: "image4.jpg",
-      img2: "image4-hover.jpg",
-      product_title: "Product Four",
-      product_desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, repellendus.",
-      product_price: "Rs. 400.00",
+      img1: coffee1,
+      img2: coffee2,
+      product_title: "Drip Solo Bag",
+      product_desc: "Drip Bags",
+      product_price: "10 – ر.ق90",
+      product_detail: "link4",
+    },
+    {
+      id: 5,
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Ethiopia – Guji",
+      product_desc: "Coffee",
+      product_price: "45 –ق140",
+    },
+    {
+      id: 6,
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Honduras Coffee",
+      product_desc: "Coffee",
+      product_price: "45 –ق140",
+    },
+    {
+      id: 7,
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "P51 Blend",
+      product_desc: "Coffee",
+      product_price: "45 –ق140",
+    },
+    {
+      id: 8,
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Panama Blend",
+      product_desc: "Coffee",
+      product_price: "45 –ق140",
+    },
+    {
+      id: 9,
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Peru Natural Coffee",
+      product_desc: "Coffee",
+      product_price: "45 –ق140",
+    },
+    {
+      id: 10,
+      img1: coffee3,
+      img2: coffee4,
+      product_title: "Peru Washed Coffee",
+      product_desc: "Coffee",
+      product_price: "45 –ق140",
     },
   ];
 
   return (
     <div className="home-product-parent parent">
       <div className="home-product-cont cont">
+        <h2>Our Product </h2>
         <Swiper
+          ref={swiperRef}
           slidesPerView={3}
           spaceBetween={10}
           pagination={{
             clickable: true,
           }}
-          modules={[Pagination]}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          modules={[Pagination, Autoplay]}
           className="mySwiper"
         >
           {products.map((item, index) => (
             <SwiperSlide key={item.id}>
-              <div
+              <Link
+                to={item.product_detail}
                 className="swiper-box"
                 onMouseEnter={() => setHoveredIndex(index)}
                 onMouseLeave={() => setHoveredIndex(null)}
               >
-                <div className="top-box">
+                <div
+                  className="top-box"
+                  onMouseEnter={() => swiperRef.current?.swiper.autoplay.stop()}
+                  onMouseLeave={() =>
+                    swiperRef.current?.swiper.autoplay.start()
+                  }
+                >
                   <div
-                    className="coffee-image bg-img-contain"
+                    className="coffee-image bg-img-cover"
                     style={{
-                      backgroundImage: `url(${hoveredIndex === index ? item.img2 : item.img1})`,
+                      backgroundImage: `url(${
+                        hoveredIndex === index ? item.img2 : item.img1
+                      })`,
                     }}
                   ></div>
-                  <Link to="#" className="btn-n">
+                  <Link to="/about" className="btn-n">
                     Add To Cart
                   </Link>
+                  {/* <Link to="#" className="favourite">
+                  <MdFavoriteBorder />
+                  </Link>
+                  <Link to="#" className="btn-search">
+                  <IoIosSearch />
+                  </Link> */}
                 </div>
                 <div className="bottom-box">
                   <div className="product-heading">{item.product_title}</div>
                   <p className="product-desc">{item.product_desc}</p>
                   <div className="product-price">{item.product_price}</div>
                 </div>
-              </div>
+              </Link>
             </SwiperSlide>
           ))}
         </Swiper>
